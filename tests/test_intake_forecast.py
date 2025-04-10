3db45442b053cf9e5aa5a75f17cdf02ed9e115e4
Rafael Guedes <r.guedes@oceanum.science>
2025-04-10 18:32:17 +1200

    Add initial files

---
+++ tests/test_intake_forecast.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""Tests for `intake_forecast` package."""
+
+import pytest
+
+
+from intake_forecast import intake_forecast
+
+
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    # import requests
+    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+
+
+def test_content(response):
+    """Sample pytest test function with the pytest fixture as an argument."""
+    # from bs4 import BeautifulSoup
+    # assert 'GitHub' in BeautifulSoup(response.content).title.string
